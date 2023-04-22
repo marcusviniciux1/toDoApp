2d3f25c3b0233dea0c4789e1575dbd3270c33f31
Marcus Vinicius <marcusvinicius.rodrigues@estudante.ufjf.br>
2023-04-21 22:38:40 -0300

    customiza a interface

--- src/main/java/view/MainScreen.java
+++ src/main/java/view/MainScreen.java
@@ -15,6 +15,8 @@ import java.util.List;
 import javax.swing.DefaultListModel;
 import model.Project;
 import model.Task;
+import util.ButtonColumnCellRenderer;
+import util.DeadlineColumnCellRenderer;
 import util.TaskTableModel;
 
 /**
@@ -32,10 +34,11 @@ public class MainScreen extends javax.swing.JFrame {
 
     public MainScreen() {
         initComponents();
-        decorateTableTask();
 
         initDataController();
         initComponentsModel();
+
+        decorateTableTask();
     }
 
     /**
@@ -384,7 +387,7 @@ public class MainScreen extends javax.swing.JFrame {
             case 5:
                 taskController.removeById(task.getId());
                 tasksModel.getTasks().remove(task);
-                
+
                 int projectIndex = jListProjects.getSelectedIndex();
                 Project project = (Project) projectsModel.get(projectIndex);
                 loadTasks(project.getId());
@@ -461,6 +464,12 @@ public class MainScreen extends javax.swing.JFrame {
         jTableTasks.getTableHeader().setForeground(new Color(255, 255, 255));
 
         jTableTasks.setAutoCreateRowSorter(true);
+        
+        jTableTasks.getColumnModel().getColumn(2).setCellRenderer(new DeadlineColumnCellRenderer());
+    
+        jTableTasks.getColumnModel().getColumn(4).setCellRenderer(new ButtonColumnCellRenderer("edit"));
+    
+        jTableTasks.getColumnModel().getColumn(5).setCellRenderer(new ButtonColumnCellRenderer("delete"));
     }
 
     public void initDataController() {
